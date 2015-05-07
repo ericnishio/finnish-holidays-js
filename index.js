65976ac7f260a4cfe6da1fa1fd61ce14d58db95a
Eric Nishio <eric@self-learner.com>
2015-05-08 00:03:00 +0300

    Remove debug code

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 var calendar = require('./lib/calendar');
 
-// Public API
 module.exports = {
   next: calendar.next,
   year: calendar.byYear,
   month: calendar.byMonth
 };
-
-var next = calendar.next(3);
-var byYear = calendar.byYear(2016, true);
-var byMonth = calendar.byMonth(1, 2015);
-
-console.log(byYear);
