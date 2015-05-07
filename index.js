06f9f7793a92f9a92f855ae30d26d4d0cfb9e6e8
Eric Nishio <eric@self-learner.com>
2015-05-08 00:35:48 +0300

    Fix public API

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var calendar = require('./lib/calendar');
 
 module.exports = {
-  next: calendar.next,
-  year: calendar.byYear,
-  month: calendar.byMonth
+  next: function(count, includeWeekends) {
+    return calendar.next(count, includeWeekends);
+  },
+  year: function(year, includeWeekends) {
+    return calendar.byYear(year, includeWeekends);
+  },
+  month: function(month, year, includeWeekends) {
+    return calendar.byMonth(month, year, includeWeekends);
+  }
 };
