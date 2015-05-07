0b6b8fc8e6dced95e8caf155f485c1b9251eb05e
Eric Nishio <eric@self-learner.com>
2015-05-07 23:34:23 +0300

    Include Pentecost

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var dateUtils = require('./lib/date-utils');
 var calendar = require('./lib/calendar');
 
 var next = calendar.next(3);
-var byYear = calendar.byYear(2015);
+var byYear = calendar.byYear(2015, true);
 var byMonth = calendar.byMonth(1, 2015);
-//console.log(byYear);
 console.log(byYear);
