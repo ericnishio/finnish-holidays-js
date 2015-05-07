18f94198951b04415a5f6b03fee3a6f18f4714fb
Eric Nishio <eric@self-learner.com>
2015-05-07 23:55:36 +0300

    Include Midsummer Eve

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-var dateUtils = require('./lib/date-utils');
 var calendar = require('./lib/calendar');
+
+// Public API
+module.exports = {
+  next: calendar.next,
+  year: calendar.byYear,
+  month: calendar.byMonth
+};
 
 var next = calendar.next(3);
 var byYear = calendar.byYear(2015, true);
 var byMonth = calendar.byMonth(1, 2015);
+
 console.log(byYear);
