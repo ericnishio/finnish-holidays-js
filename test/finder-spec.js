bd38cc40ae85c74561177f27176049fb06263e75
Eric Nishio <ericnishio@protonmail.com>
2016-05-07 23:39:36 +0300

    Add support for finding previous holiday by date and index

--- test/finder-spec.js
+++ test/finder-spec.js
@@ -3,11 +3,44 @@ var finder = require('../lib/finder');
 
 describe('finder', function() {
   it('should find next holiday', function() {
+    var nextHoliday = finder.after(6, 2016, 0);
+
+    expect(nextHoliday).to.eql({
+      description: 'Independence Day',
+      day: 6,
+      month: 12,
+      year: 2016
+    });
+  });
+
+  it('should find another next holiday', function() {
     var nextHoliday = finder.after(12, 2016, 0);
 
     expect(nextHoliday).to.eql({
-      day: 26,
       description: "St. Stephen's Day",
+      day: 26,
+      month: 12,
+      year: 2016
+    });
+  });
+
+  it('should find previous holiday', function() {
+    var nextHoliday = finder.before(12, 2016, 0);
+
+    expect(nextHoliday).to.eql({
+      description: 'Midsummer Eve',
+      day: 24,
+      month: 6,
+      year: 2016
+    });
+  });
+
+  it('should find another previous holiday', function() {
+    var nextHoliday = finder.before(12, 2016, 1);
+
+    expect(nextHoliday).to.eql({
+      description: 'Independence Day',
+      day: 6,
       month: 12,
       year: 2016
     });
